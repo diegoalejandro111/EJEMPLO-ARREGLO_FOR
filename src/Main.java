ed045faaad2a96ccb4305e2f801fa50aabd719e3
diegoalejandro111 <146129565+diegoalejandro111@users.noreply.github.com>
2023-10-17 16:57:02 -0500

    PPP

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+
+public class Main {
+    public static void main(String[] args) {
+        // Declarar un arreglo de números enteros
+        int[] numeros = {1, 2, 3, 4, 5};
+
+        // Usar un ciclo for para recorrer el arreglo
+        for (int i = 0; i < numeros.length; i++) {
+            System.out.println(numeros[i]);
+        }
+    }
+}
